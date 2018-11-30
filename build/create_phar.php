043191e9acb0b0aaefaf32f1c4a14acabd8b8fe8
talisto <talisto@gmail.com>
2018-11-30 00:44:23 -0800

    update dependencies

--- build/create_phar.php
+++ build/create_phar.php
@@ -1,8 +1,7 @@
 <?php
 
-$phar = new Phar(__DIR__ . "/cpvc.phar", 0, "cpvc.phar");
+$phar = new Phar("cpvc.phar", 0, "cpvc.phar");
 $phar->buildFromDirectory(realpath(__DIR__.'/../'), '/\.(php|json)$/');
 $stub = $phar->createDefaultStub("build/phar_bootstrap.php");
-$stub = "#!/usr/bin/php \n".$stub;
+$stub = "#!/usr/bin/env php\n".$stub;
 $phar->setStub($stub);
-$phar->convertToExecutable();
